d2973d111b090d2f68512dd41c85e04cfab16594
westorres9 <wester.torres9@gmail.com>
2022-06-17 09:41:42 -0300

    projeto pronto para deploy

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -13,11 +13,11 @@ import java.util.List;
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
 
-    @Query("SELECT new com.devsuperior.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+    @Query(value = "SELECT new com.devsuperior.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
         + "FROM Sale AS obj GROUP BY obj.seller")
     List<SaleSumDTO> amountGroupedBySeller();
 
-    @Query("SELECT new com.devsuperior.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+    @Query(value = "SELECT new com.devsuperior.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
             + "FROM Sale AS obj GROUP BY obj.seller")
     List<SaleSuccessDTO> successGroupedBySeller();
 }
